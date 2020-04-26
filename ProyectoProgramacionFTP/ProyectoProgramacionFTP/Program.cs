293ca74e0bd6f8aeda613143c5ee33b5de813554
sebastian.villada <jsebastian1422@gmail.com>
2020-04-26 10:52:05 -0500

    Ajustes en el cambio proceso de movimiento archivos

--- ProyectoProgramacionFTP/ProyectoProgramacionFTP/Program.cs
+++ ProyectoProgramacionFTP/ProyectoProgramacionFTP/Program.cs
@@ -23,6 +23,7 @@ namespace ConsoleApp1
         static void Main()
         {
             MoverArchivosFTP();
+            //ConverXmlFiles.Program convert = new ConverXmlFiles.Program();
         }
 
         public static void CrearExcelPrueba()
@@ -34,49 +35,26 @@ namespace ConsoleApp1
         //Este Metodo lo que hace es mover los archivos 
         public static void MoverArchivosFTP()
         {
-            string NombreArchivo = "prueba_1.csv";
-            string CarpOrigen = @"C:\Users\Windows\Desktop\Programacion C·\ProyectoFinalFTP\archivos\archivoCVS";
-            string CarpDestino = @"C:\Users\Windows\Desktop\Programacion C·\ProyectoFinalFTP\archivos";
-            string sourceFile = System.IO.Path.Combine(CarpOrigen, NombreArchivo);
-            string destFile = System.IO.Path.Combine(CarpDestino, NombreArchivo);
+            string fullPath = @"..\..\";
+            string directorioDesctino = Path.GetFullPath(fullPath + "/Documentos/DocumentosCSV");
+            string directorioOrigen = Path.GetFullPath(fullPath + "/Documentos/GeneraDocumentosCSV");
 
-            System.IO.Directory.CreateDirectory(CarpDestino);
-            System.IO.File.Copy(sourceFile, destFile, true);
-
-
-            if (System.IO.Directory.Exists(CarpOrigen))
+            if (!System.IO.Directory.Exists(directorioDesctino))
             {
-                string[] files = System.IO.Directory.GetFiles(CarpOrigen);
-
-                // Copy the files and overwrite destination files if they already exist.
-                foreach (string s in files)
+                System.IO.Directory.CreateDirectory(System.IO.Path.Combine(directorioDesctino, ""));
+            }
+            if (System.IO.Directory.Exists(directorioOrigen))
+            {
+                string[] directory = System.IO.Directory.GetFiles(@directorioOrigen);
+                foreach (var files in directory)
                 {
-                    // Use static Path methods to extract only the file name from the path.
-                    Thread.Sleep(600);
-                    NombreArchivo = System.IO.Path.GetFileName(s);
-                    destFile = System.IO.Path.Combine(CarpDestino, NombreArchivo);
-                    System.IO.File.Copy(s, destFile, true);
-                    Console.WriteLine("Trasferiendo el Archivo.................... " + NombreArchivo);
-                }
-                //File.Delete(destFile);
-                if (Directory.Exists(CarpOrigen))
-                {
-                    foreach (var item in Directory.GetFiles(CarpOrigen, "*.*"))
-                    {
-                        NombreArchivo = System.IO.Path.GetFileName(item);
-                        File.SetAttributes(item, FileAttributes.Normal);
-                        Thread.Sleep(600);
-                        File.Delete(item);
-                        Console.WriteLine("Eliminando el Archivo...................." + NombreArchivo);
-                    }
+                    Thread.Sleep(2000);
+                    string nameFile = System.IO.Path.GetFileName(files);
+                    string fileDestino = (System.IO.Path.Combine(directorioDesctino, nameFile));
+                    System.IO.File.Move(files, fileDestino);
+                    Console.WriteLine("Transfiriendo archivos CSV a la carpeta Origen.................... " + nameFile);
                 }
             }
-            else
-            {
-                Console.WriteLine("No Existe El Archivo");
-            }
-
-
         }
 
 
