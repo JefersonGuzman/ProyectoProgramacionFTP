0a7e409bb141597680164a9cb8bad794e8de5ae0
JeffGuzman <jefersonlozano07@gmail.com>
2020-06-01 20:56:24 -0500

    creamos las colas de las prioridades

---
+++ ProyectoProgramacionFTP/ProyectoProgramacionFTP/Colas/ColaPrioridadMedia.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoProgramacionFTP.Colas
+{
+    class Class1
+    {
+    }
+}
